d75aaf59adc0fe6141ebeb01768d9818d2fd2022
liuqingxian1994 <377253672@qq.com>
2019-05-03 23:24:29 +0800

    201905032324

---
+++ PycharmProjects/untitled/语言元素/math.py
@@ -0,0 +1,78 @@
+"""
+使用变量保存数据并进行算术运算
+
+"""
+
+a = 321
+b = 123
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+print(2 ** 5)
+
+
+"""
+使用input函数输入
+使用int()进行类型转换
+用占位符格式化输出的字符串
+
+"""
+
+a = int(input('a = '))
+b = int(input('b = '))
+print('%d + %d = %d' % (a, b, a + b))
+print('%d - %d = %d' % (a, b, a - b))
+print('%d * %d = %d' % (a, b, a * b))
+print('%d / %d = %f' % (a, b, a / b))
+print('%d // %d = %d' % (a, b, a // b))
+print('%d %% %d = %d' % (a, b, a % b))
+print('%d ** %d = %d' % (a, b, a ** b))
+
+
+"""
+使用type()检查变量的类型
+
+"""
+
+a = 100
+b = 12.345
+c = 1 + 5j
+d = 'hello, world'
+e = True
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+
+
+"""
+运算符的使用
+"""
+
+a = 5
+b = 10
+c = 3
+d = 4
+e = 5
+a += b
+a -= c
+a *= d
+a /= e
+print("a = ", a)
+
+flag1 = 3 > 2
+flag2 = 2 < 1
+flag3 = flag1 and flag2
+flag4 = flag1 or flag2
+flag5 = not flag1
+print("flag1 = ", flag1)
+print("flag2 = ", flag2)
+print("flag3 = ", flag3)
+print("flag4 = ", flag4)
+print("flag5 = ", flag5)
+print(flag1 is True)
+print(flag2 is not False)
